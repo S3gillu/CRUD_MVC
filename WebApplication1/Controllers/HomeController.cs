a883efc168e400768cfd65172cc97343e31a41f2
S3gillu <shubhamsaurabh440@yahoo.com>
2017-09-27 15:04:27 +0530

    This is commited

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Employee> shu;
+        public HomeController()
+        {
+            shu = new List<Employee>()
+            {
+                new Employee()
+            { EmployeeId = 1 , FirstName="Shubham",LastName="Saurabh", Email="shubhamsaurabh@gmail.com" },
+            new Employee()
+            { EmployeeId = 2 , FirstName="Mayank",LastName="Joshi", Email="mayankjoshi@gmail.com" },
+            new Employee()
+            { EmployeeId = 3 , FirstName="Aman",LastName="Mian", Email="amanmian@yahoo.com" },
+            new Employee()
+            { EmployeeId = 4 , FirstName="Praveen",LastName="Bhatt", Email="praveenbhatt@gmail.com" },
+            new Employee()
+            { EmployeeId = 5 , FirstName="Aniket",LastName="kadu", Email="aniketkadu@gmail.com"},
+
+            };
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(shu);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ShubhamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ShubhamController : Controller
+    {
+        // GET: Shubham
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
